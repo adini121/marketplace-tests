73f6f6bf0dfb8da9c2e10fa83f5fa3ef50dd6d9f
AdiMac <adamsken1221@gmail.com>
2015-11-30 18:05:40 +0100

    Add sessionID and time to mysql table: bugfix

--- conftest.py
+++ conftest.py
@@ -88,7 +88,7 @@ def session_id(mozwebqa):
         print('............Successfully created table .......')
         insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
         # insQuery = """insert into test_session_ids (session_id, date_created) values ('whatever', 'whatever')"""
-        c.execute("insert into test_session_ids (session_id, date_created) values ('%s', '%s')", (str_session_id, current_time))
+        c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
         # c.execute(insQuery)
         print('............Successfully ADDED to table .......')
         conn.commit()
